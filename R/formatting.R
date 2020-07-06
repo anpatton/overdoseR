4cba9c98bc1d8812c51242ec75b2903febb7e68f
Andrew Patton <anpatt7@gmail.com>
2020-07-06 09:53:03 -0700

    fixed age filter

--- R/formatting.R
+++ R/formatting.R
@@ -220,7 +220,7 @@ format_multirow_ems_data <- function(data_in,
     dplyr::ungroup() %>%
     dplyr::mutate(cardiac_flag = ifelse((grepl("cardiac", unlist(.[primary_impression_name]), ignore.case = TRUE) == TRUE
                                          & opioid_agonist_success != 1), 1, 0)) %>%
-    dplyr::mutate(age_flag = ifelse((unlist(.[patient_age_name]) < 18 | unlist(.[patient_age_name]) > 70
+    dplyr::mutate(age_flag = ifelse(((unlist(.[patient_age_name]) < 18 | unlist(.[patient_age_name]) > 70)
                                      & opioid_agonist_success != 1), 1, 0)) %>%
     dplyr::mutate(cardiac_flag = ifelse(is.na(cardiac_flag) == TRUE, 0, cardiac_flag)) %>%
     dplyr::mutate(age_flag = ifelse(is.na(age_flag) == TRUE, 0, cardiac_flag))
